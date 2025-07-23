b38ebbda5f258a1284238caadc1d647e9e2f7c12
GigiNL11 <165532141+GigiNL11@users.noreply.github.com>
2025-07-23 19:16:05 -0300

    Update shift_left_2.v
    Cabeçalho em shift_left_2.v

--- shift_left_2.v
+++ shift_left_2.v
@@ -1,3 +1,28 @@
+/*
+================================================================================
+==                        Informações do Projeto                        ==
+================================================================================
+Disciplina:   Arquitetura e Organização de Computadores (2025.1)
+Professor:    Vítor A. Coutinho
+Projeto:      Implementação de um Processador MIPS Monociclo (Projeto da 2ª VA)
+Grupo:
+              - Giovanna Neves
+              - João Pedro Oliveira da Silva
+              - José Albérico
+              - Lucas Lins
+
+================================================================================
+==                        Informações do Arquivo                        ==
+================================================================================
+Arquivo:      shift_left_2.v
+Módulo:       shift_left_2
+
+Descrição:
+              Módulo que realiza um deslocamento lógico de 2 bits para a esquerda
+              (shift left 2).
+================================================================================
+*/
+
 module shift_left_2 (in, out);
 
 // Descrição das portas
